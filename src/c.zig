c5d5f36e125b5a1c4a1eae08bb63f7eea6188a1b
verontrix <garretrgang@gmail.com>
2025-03-08 11:32:44 -0800

    sdl3_image uses all your belongs to us to build SDL3_Image

--- src/c.zig
+++ src/c.zig
@@ -1,4 +1,7 @@
 pub const C = @cImport({
     @cInclude("SDL3/SDL.h");
-    // @cInclude("SDL3_image/SDL_image.h");
+    const ext_image = @import("extension_options").image; //optional include 
+    if (ext_image) {
+        @cInclude("SDL3_image/SDL_image.h");
+    }
 });
