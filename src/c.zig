c4cd653af7be0b37f81fd1625523c74281a543b3
verontrix <garretrgang@gmail.com>
2025-03-10 22:26:57 -0700

    finished rebase

--- src/c.zig
+++ src/c.zig
@@ -1,5 +1,6 @@
 pub const C = @cImport({
     @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_vulkan.h");
     const ext_image = @import("extension_options").image; //optional include 
     if (ext_image) {
         @cInclude("SDL3_image/SDL_image.h");
